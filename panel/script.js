91a3a0a7aa44461761bc4b298555f9b561eacd74
treppi✨ <53356121+Treppenhaus@users.noreply.github.com>
2024-10-12 20:20:04 +0200

    first design layout

---
+++ panel/script.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+addObject = (type, data) => {
+    if(type == "button") {
+        let content = document.getElementById("content");
+
+        
+    }
+    else return;
+}
+
+
+
+addObject("button", {
+    send: "10.0.100.36/sh/test_send",
+    recv: "10.0.100.36/sh/test_recv"
+})
